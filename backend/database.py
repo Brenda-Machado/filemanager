3320ec0c67c0f7f46f4e0080adf478d9e6524cc0
Brenda-Machado <araucaria2011@gmail.com>
2026-03-09 15:39:42 -0300

    docker adjustments db path

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ File Manager - database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = 'sqlite:///./filemanager.db'
+DATABASE_URL = "sqlite:////app/db/filemanager.db"
 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
