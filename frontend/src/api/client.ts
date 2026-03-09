de4cb6b2ae6679cec3576327dcf37c1270480125
Brenda-Machado <araucaria2011@gmail.com>
2026-03-09 15:53:01 -0300

    removing virtual env from dockerfile

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000",
+  baseURL: "http://localhost:8000",
   timeout: 30000,
 });
 
